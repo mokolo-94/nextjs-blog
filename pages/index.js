04766a3781b391f06f6b3b7e151167d178db5731
mokolo-94 <86370796+mokolo-94@users.noreply.github.com>
2022-01-10 12:06:10 +0100

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
-import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utilss.module.css';
 import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link'
-import Date from '../components/date'
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
 
 export default function Home({ allPostsData }) {
@@ -25,17 +24,17 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,40 +1,41 @@
 import Layout from '../../components/layout';
+import Home from '..';
 import { getAllPostIds, getPostData } from '../../lib/posts';
-import Date from '../../components/date'
+import Date from '../../components/date';
 import Head from 'next/head';
-import utilStyles from '../../styles/utilss.module.css'
-
+import utilStyles from '../../styles/utilss.module.css';
 
 export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id)
-      return {
+  const postData = await getPostData(params.id);
+  return {
     props: {
-      postData
-    }
-  }
+      postData,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = getAllPostIds();
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        <Head>
-          <title>{postData.title}</title>
-        </Head>
-        <article>
-          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-          <div className={utilStyles.lightText}>
-            <Date dateString={postData.date} />
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
-      </Layout>
-    )
-  }
+  console.log(postData);
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
